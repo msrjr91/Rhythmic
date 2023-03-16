870f78b930fa80e4c074e0573bbfc4764abb57b3
meixingc <realmeixing@gmail.com>
2023-03-16 15:43:51 -0400

    model users association change

--- server/models/users.js
+++ server/models/users.js
@@ -15,7 +15,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId'
       })
       Users.hasOne(models.Artists, {
-        foreignKey: 'name'
+        foreignKey: 'userId'
       })
     }
   }
