d557d5ddba0d5736dc935f25967a5fd8001b5587
meixingc <realmeixing@gmail.com>
2023-03-16 15:43:20 -0400

    added user id to create user migration

--- server/migrations/20230316190404-create-artists.js
+++ server/migrations/20230316190404-create-artists.js
@@ -18,6 +18,9 @@ module.exports = {
       fans: {
         type: Sequelize.INTEGER
       },
+      userId: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
