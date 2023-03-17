8846cbcf52abd533eed6f5e56466affbfed4b74d
Ariana Jackson <ariana.desiree@gmail.com>
2023-03-17 09:53:22 -0500

    search bar partial

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,7 +1,11 @@
+import SearchBar from "./SearchBar";
+
 export default function Nav() {
     return (
         <div>
-        <p>im nav</p>
+        <p>im nav
+            <SearchBar />
+        </p>
         </div>
     )
 }
---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useState } from "react";
+
+
+const SearchBar = () => {
+    const [query, setQuery] = useState("")
+
+
+
+    return (
+        <div className="searchbar">
+            <input type="text" onChange={e => setQuery(e.target.value)} />
+
+            <button type='button' className="searchbtn"
+            // onClick={searchMeal}
+            > Search </button>
+
+        </div>
+    
+
+    )
+}
+
+export default SearchBar
