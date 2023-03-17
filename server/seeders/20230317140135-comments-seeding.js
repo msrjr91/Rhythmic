27befa32cee1e49624c107d4937b84d782884fc7
meixingc <realmeixing@gmail.com>
2023-03-17 10:34:05 -0400

    comments seed changes

--- server/seeders/20230317140135-comments-seeding.js
+++ server/seeders/20230317140135-comments-seeding.js
@@ -1,27 +1,26 @@
-'use strict';
+'use strict'
 const { Users, Posts, sequelize } = require('../models')
-const falso = require("@ngneat/falso");
+const falso = require("@ngneat/falso")
 
-/** @type {import('sequelize-cli').Migration} */
+const comments = await Promise.all(
+  [...Array(15)].map(async () => {
+    let user = await Users.findOne({ order: sequelize.random(), raw: true })
+    let post = await Posts.findOne({ order: sequelize.random(), raw: true })
+    return {
+      userId: user.id,
+      postId: post.id,
+      content: falso.randLine,
+      createdAt: new Date,
+      updatedAt: new Date
+    }
+  })
+)
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const comments = await Promise.all(
-      [...Array(15)].map(async () => {
-        let user = await Users.findOne({ order: sequelize.random(), raw: true })
-        let post = await Posts.findOne({ order: sequelize.random(), raw: true })
-        return {
-          userId: user.id,
-          postId: post.id,
-          content: falso.randLine,
-          createdAt: new Date,
-          updatedAt: new Date
-        }
-      })
-    )
-  return queryInterface.bulkInsert("comments", comments)
+    return queryInterface.bulkInsert("comments", comments)
   },
-
   async down (queryInterface, Sequelize) {
     return queryInterface.bulkDelete('comments')
   }
-};
+}
