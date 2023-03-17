e1482da9ae20e5de0aa59901e4169f0b0e477f1c
meixingc <realmeixing@gmail.com>
2023-03-17 09:10:00 -0400

    artist model associations

--- server/models/artists.js
+++ server/models/artists.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Artists.belongsTo(models.Users, {
+        foreignKey: 'userId'
+      })
     }
   }
   Artists.init({
