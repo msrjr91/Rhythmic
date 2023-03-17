3ca38a8e3966b2959f2d12ddeedf7a0fc12bc89d
ralp22 <ryan_halpert@yahoo.com>
2023-03-17 15:30:40 -0400

    added profile page with route and button home page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,18 @@
+export default function Profile(){
+    
+    return (
+        <div>
+        <h1> this is a user profile </h1>
+        <section> 
+            <div>Username display</div>
+        </section>
+        <section>
+            <div>Following/Liked Artists/Fans</div>
+        </section>
+        <section>
+            <div>Albums/Songs/Playlists</div>
+            <div>Comments for selected song/album/playlist</div>
+        </section>
+        </div>
+    )
+}
