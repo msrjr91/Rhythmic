4a73c9811a41248181ca9d198afc107ca8e39522
meixingc <realmeixing@gmail.com>
2023-03-17 09:23:18 -0400

    track model associations

--- server/models/tracks.js
+++ server/models/tracks.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Tracks.belongsToMany(models.Posts, {
+        foreignKey: 'trackId'
+      })
     }
   }
   Tracks.init({
