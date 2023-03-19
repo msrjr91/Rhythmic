6792efe0bbbab7906a79e82691e89dd311b9d1fe
meixingc <realmeixing@gmail.com>
2023-03-19 18:29:24 -0400

    layout edits

--- server/seeders/2-postseeds.js
+++ server/seeders/2-postseeds.js
@@ -1,6 +1,7 @@
-'use strict';
+'use strict'
 const { Users, Tracks, sequelize } = require('../models')
 const falso = require('@ngneat/falso')
+
 const posts = [...Array(15)].map(async () => {
         let user = await Users.findOne({ order: sequelize.random(), raw: true })
         let track = await Tracks.findOne({ order: sequelize.random(), raw: true })
@@ -14,7 +15,7 @@ const posts = [...Array(15)].map(async () => {
       })
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert("posts", posts)
+    return queryInterface.bulkInsert('posts', posts)
   },
   async down (queryInterface, Sequelize) {
     return queryInterface.bulkDelete('posts')
