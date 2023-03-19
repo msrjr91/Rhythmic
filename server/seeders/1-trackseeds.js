0a25e2f9a497c4f0e5271deb4fa6eb03ff2c05c6
Michael <michael.s.ross10@gmail.com>
2023-03-19 15:35:45 -0600

    edited seed files

---
+++ server/seeders/1-trackseeds.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const falso = require('@ngneat/falso')
+const tracks = [...Array(15)].map(() => ({
+    trackURI: falso.randNumber(),
+    title: falso.randCatchPhrase(),
+    artist: falso.randFullName(),
+    createdAt: new Date,
+    updatedAt: new Date
+}))
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("tracks", tracks)
+  },
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('tracks')
+  }
+}
--- server/seeders/2-postseeds.js
+++ server/seeders/2-postseeds.js
@@ -1,24 +1,20 @@
 'use strict';
 const { Users, Tracks, sequelize } = require('../models')
 const falso = require('@ngneat/falso')
-
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    const posts = await Promise.all(
-      [...Array(15)].map(async () => {
+const posts = [...Array(15)].map(async () => {
         let user = await Users.findOne({ order: sequelize.random(), raw: true })
         let track = await Tracks.findOne({ order: sequelize.random(), raw: true })
         return {
           userId: user.id,
-          content: falso.randLine,
+          content: falso.randLine(),
           trackId: track.id,
           createdAt: new Date,
           updatedAt: new Date
         }
       })
-    )
-  return queryInterface.bulkInsert("posts", posts)
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert("posts", posts)
   },
   async down (queryInterface, Sequelize) {
     return queryInterface.bulkDelete('posts')
