cc54d4c80e1a5f702bd946bc7979bf57fd1a6fb5
Michael <michael.s.ross10@gmail.com>
2023-03-20 13:14:56 -0600

    edited user controllers

--- server/routes/UserRouter.js
+++ server/routes/UserRouter.js
@@ -2,5 +2,6 @@ const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
 Router.get('/', controller.GetAllUsers)
-
+Router.get('/followers', controller.GetUserFollowers)
+Router.get('/following', controller.GetUserFollowing)
 module.exports = Router
