9619239216803bf7395b52c53b13acc6cbd29371
Michael <michael.s.ross10@gmail.com>
2023-03-20 21:58:39 -0600

    still working on follower controllers

--- server/controllers/FollowerController.js
+++ server/controllers/FollowerController.js
@@ -9,6 +9,64 @@ const GetFollowers = async (req, res) => {
   }
 }
 
+//RETURNS ALL OF USERID's FOLLOWERS: [1,2,...n] follows userId
+const GetFollowersByUser = async (req, res) => {
+  try {
+    const followers = await Followers.findAll({
+      where: {
+        userId: req.params.user_id
+      }
+    })
+    res.send(followers)
+  } catch (error) {
+    throw error
+  }
+}
+
+//RETURNS ALL USERS THAT USERID FOLLOWS:  userId follows [1,2,...n]
+const GetFollwersByFollower = async (req,res) => {
+  try {
+    const followers = await Followers.findAll({
+      where: {
+        followerId: req.params.follower_id
+      }
+    })
+    res.send(followers)
+  } catch (error) {
+    throw error
+  }
+}
+
+//"I (FOLLOWER ID) WANT TO FOLLOW SOME USER (USER ID)"
+const FollowUser = async (req,res) => {
+  try {
+    let followerId = parseInt(req.params.follower_id)
+    let followBody = {
+      followerId,
+      ...req.body
+    }
+    let follow = await Followers.create(followBody)
+    res.send(follow)
+  } catch (error) {
+    throw error
+  }
+}
+
+// //REMOVE FOLLOW
+// const UnfollowUser = async (req,res) => {
+//   try {
+//     //user you want to unfollow
+//     let userId = parseInt(req.params.user_id)
+//     //
+//     await Followers.destroy({where: {userId: }})
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
 module.exports = {
   GetFollowers,
+  GetFollowersByUser,
+  GetFollwersByFollower,
+  FollowUser
 }
--- server/routes/FollowerRouter.js
+++ server/routes/FollowerRouter.js
@@ -2,5 +2,11 @@ const Router = require('express').Router()
 const controller = require('../controllers/FollowerController')
 
 Router.get('/', controller.GetFollowers)
+//RETURNS ALL OF USERID's FOLLOWERS: [1,2,...n] follows userId
+Router.get('/user/:user_id', controller.GetFollowersByUser)
+//RETURNS ALL USERS THAT USERID FOLLOWS:  userId follows [1,2,...n]
+Router.get('/follower/:follower_id', controller.GetFollwersByFollower)
+
+Router.post('/follower/:follower_id', controller.FollowUser)
 
 module.exports = Router
