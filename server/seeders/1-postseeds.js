f76e20c4ed5d29202046d857058ff596c85403e5
Michael <michael.s.ross10@gmail.com>
2023-03-20 19:12:23 -0600

    fixed seed files to randomize user and post id

--- server/seeders/1-postseeds.js
+++ server/seeders/1-postseeds.js
@@ -7,14 +7,17 @@ const falso = require('@ngneat/falso')
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    let user = await Users.findOne({ order: sequelize.random(), raw: true })
-    const posts = [...Array(100)].map(() => ({
-      userId: user.id,
+    let user = await Users.findAll({raw:true})
+    const posts = [...Array(100)].map(() => {
+      let r = Math.floor(Math.random() * user.length)
+      return {
+      userId: user[r].id,
       content: falso.randLine(),
       trackId: falso.randUuid(),
       createdAt: new Date(),
       updatedAt: new Date()
-  }))
+      }
+  })
     return queryInterface.bulkInsert('posts', posts)
   },
   async down (queryInterface, Sequelize) {
