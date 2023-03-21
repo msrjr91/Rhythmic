bcad1e5bdae5236aa93941abf0aed8427994ef68
Michael <michael.s.ross10@gmail.com>
2023-03-20 21:35:05 -0600

    finished comment routers and controllers for CRUD

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -9,6 +9,61 @@ const GetComments = async (req, res) => {
   }
 }
 
+const GetCommentsByUser = async (req,res) => {
+  try {
+    const comments = await Comments.findAll({
+      where: {
+        userId: req.params.user_id
+      }
+    })
+    res.send(comments)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetCommentsByPost = async (req,res) => {
+  try {
+    const comments = await Comments.findAll({
+      where: {
+        postId: req.params.post_id
+      }
+    })
+    res.send(comments) 
+  } catch (error) {
+    throw error
+  }
+}
+
+//NEED TO HARDCODE IN USERID ON INSOMNIA FOR NOW UNTIL WE CAN LINK TO FRONTEND LOGIN USER ACCOUNT
+const CreateComment = async (req,res) => {
+  try {
+    let postId = parseInt(req.params.post_id)
+    let commentBody = {
+      postId,
+      ...req.body
+    }
+    let comment = await Comments.create(commentBody)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteComment = async (req,res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    await Comments.destroy({where: {id: commentId}})
+    res.send({message: `Deleted comment id ${commentId}`})
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
   GetComments,
+  GetCommentsByUser,
+  GetCommentsByPost,
+  CreateComment,
+  DeleteComment
 }
