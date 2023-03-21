fa99dd83a1fc47d56c27ce4ae6e2975430ea4981
meixingc <realmeixing@gmail.com>
2023-03-21 17:48:09 -0400

    css changes

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,19 +1,25 @@
 import {useNavigate} from 'react-router-dom'
 import SearchBar from './SearchBar'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
     let navigate = useNavigate()
     return (
         <div className="nav">
-            <SearchBar />
+            <div className='nav-left'>
+                <SearchBar />
+            </div>
             <div className="nav-center">
                 <img className="logo" src="/images/rhythmic.png" alt="" />
                 <div className="center-bottom">
-                    <button onClick={()=>navigate('/')}>Home</button>
-                    <button onClick={()=>navigate('/profile')}>Profile</button>
+                    <button className="registerbtn" onClick={() => navigate('/register')}>What's Your Rhythm?</button>
                 </div>
             </div>
-            <button onClick={()=>navigate('/signin')}>Sign in</button>
+            <div className='nav-right'>
+                <Link className='nav-right-buttons' onClick={()=>navigate('/')}>Home</Link>
+                <Link className='nav-right-buttons' onClick={()=>navigate('/profile')}>Profile</Link>
+                <Link className='nav-right-buttons' onClick={()=>navigate('/signin')}>Sign in</Link>
+            </div>
         </div>
     )
 }
