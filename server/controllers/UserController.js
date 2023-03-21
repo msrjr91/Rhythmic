76f15636ba7ef52a60402cb999afc997db5481ab
Michael <michael.s.ross10@gmail.com>
2023-03-20 20:48:15 -0600

    added routes for CRUD operations for user model

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,7 @@
-const { Users, Posts, Followers } = require('../models')
+const { Users, Posts, Comments, Followers } = require('../models')
+// const { Op } = require('sequelize')
 
-const GetAllUsers = async(req,res) => {
+const GetAllUsers = async (req,res) => {
   try {
     const users = await Users.findAll()
     res.send(users)
@@ -9,7 +10,16 @@ const GetAllUsers = async(req,res) => {
   }
 }
 
-const GetUserFollowers = async (req, res) => {
+const GetOneUser = async (req,res) => {
+  try {
+    let user = await Users.findByPk(req.params.user_id)
+    res.send(user)
+  } catch (error) {
+    throw error
+  }
+}
+
+const GetAllUserFollowers = async (req, res) => {
   try {
     let data = await Users.findAll({
       include: [{model: Users, as: 'followers', attributes: ['id', 'username']}]
@@ -20,7 +30,27 @@ const GetUserFollowers = async (req, res) => {
   }
 }
 
-const GetUserFollowing = async (req, res) => {
+// const GetOneUserFollowers = async (req,res) => {
+//   try {
+//     let data = await Users.findByPk(req.params.user_id, {
+//       // where: {
+//       //   [Op.is]: req.params.user_id,
+//       // },
+//       include: [{
+//         model: Followers,
+//         as: 'followers',
+//         where: {
+//           [Op.is]: req.params.user_id,
+//         }
+//       }]
+//     })
+//     res.send(data)
+//   } catch (error) {
+//     throw error
+//   }
+// }
+
+const GetAllUserFollowing = async (req, res) => {
   try {
     let data = await Users.findAll({
       include: [{model: Users, as: 'following', attributes: ['id', 'username']}]
@@ -31,7 +61,7 @@ const GetUserFollowing = async (req, res) => {
   }
 }
 
-const GetUserPosts = async (req,res) => {
+const GetAllUserPosts = async (req,res) => {
   try {
     let data = await Users.findAll({
       include: [{model: Posts}]
@@ -42,10 +72,61 @@ const GetUserPosts = async (req,res) => {
   }
 }
 
+const GetAllUserComments = async (req,res) => {
+  try {
+    let data = await Users.findAll({
+      include: [{model: Comments}]
+    })
+    res.send(data)
+  } catch (error){
+    throw error
+  }
+}
+
+const CreateUser = async (req,res) => {
+  try {
+    let userBody = {
+      ...req.body
+    }
+    let userEntry = await Users.create(userBody)
+    res.send(userEntry)
+  } catch (error) {
+    throw error
+  } 
+}
+
+const UpdateUser = async (req, res) => {
+  try {
+    let user_id = parseInt(req.params.user_id)
+    let updatedUser = await Users.update(req.body, {
+      where: {id: user_id},
+      returning: true
+    })
+    res.send(updatedUser)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteUser = async (req,res) => {
+  try {
+    let user_id = parseInt(req.params.user_id)
+    await Users.destroy({where: {id: user_id}})
+    res.send({message: `Deleted user with an id of ${user_id}`})
+  } catch (error) {
+    throw error
+  }
+}
 
 module.exports = {
   GetAllUsers,
-  GetUserFollowers,
-  GetUserFollowing,
-  GetUserPosts
+  GetOneUser,
+  GetAllUserFollowers,
+  // GetOneUserFollowers,
+  GetAllUserFollowing,
+  GetAllUserPosts,
+  GetAllUserComments,
+  CreateUser,
+  UpdateUser,
+  DeleteUser
 }
